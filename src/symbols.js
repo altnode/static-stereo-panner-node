ffe6ce425c4b96f3258f753a9ee334ccb25adddc
mohayonao <mohayonao@gmail.com>
2015-10-16 17:30:42 +0900

    add StaticStereoPannerNode

---
+++ src/symbols.js
@@ -0,0 +1,5 @@
+export const PAN = typeof Symbol !== "undefined" ? Symbol("PAN") : "altnode.static-stereo-panner-node:PAN";
+export const SPLITTER = typeof Symbol !== "undefined" ? Symbol("SPLITTER") : "altnode.static-stereo-panner-node:SPLITTER";
+export const GAIN_L = typeof Symbol !== "undefined" ? Symbol("GAIN_L") : "altnode.static-stereo-panner-node:GAIN_L";
+export const GAIN_R = typeof Symbol !== "undefined" ? Symbol("GAIN_R") : "altnode.static-stereo-panner-node:GAIN_R";
+export const MERGER = typeof Symbol !== "undefined" ? Symbol("MERGER") : "altnode.static-stereo-panner-node:MERGER";
